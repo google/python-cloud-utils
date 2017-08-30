ef65ec5d47fe7966279c85e1d5ffff48191fe84d
Arie Abramovici <beast@google.com>
2017-08-30 14:39:38 +0300

    Hard coded version

--- setup.py
+++ setup.py
@@ -13,10 +13,8 @@
 # limitations under the License.
 
 from setuptools import setup
-import subprocess
 
-COMMIT_COUNT = subprocess.check_output("git rev-list HEAD --count", shell=True)
-VERSION = '1.0.{}'.format(COMMIT_COUNT)
+VERSION = '1.0.13'
 setup(name='cloud-utils',
       version=VERSION,
       description='Python Cloud Utilities',
