bbf102845be38587446e01cae0f2aeaee79dc9f9
Arie Abramovici <beast@google.com>
2017-12-04 10:20:14 +0200

    Fix dependencies

--- setup.py
+++ setup.py
@@ -23,5 +23,5 @@ setup(name='cloud-utils',
       packages=['cloud_utils'],
       entry_points={'console_scripts': ['list_instances = cloud_utils.list_instances:main']},
       install_requires=['boto', 'boto3', 'botocore', 'google-api-python-client==1.6.2',
-                        'google.auth', 'texttable', 'futures', 'python-dateutil', 'pytz'],
+                        'google-auth', 'texttable', 'futures', 'python-dateutil', 'pytz'],
       zip_safe=False)
